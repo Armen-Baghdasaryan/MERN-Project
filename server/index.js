513e7b92e147af47accac9dea8a5fbb4123efe21
Armen <armen.baghdasaryan91@gmail.com>
2022-12-31 12:47:40 +0400

    feature/change server

--- server/index.js
+++ server/index.js
@@ -17,37 +17,6 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
-// Middleware
-app.use(cors());
-app.use(fileUpload());
-app.use(express.json());
-app.use(express.static("uploads"));
-
-// Routes
-
-app.get("/", (req, res) => {
-  res.send("Server is already Connected");
-});
-
-// http://localhost:3002
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/comments", commentRoute);
-
-// async function start() {
-//   try {
-//     await mongoose.connect(
-//       // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fxlwlgn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
-//       // `mongodb+srv://test:test123@cluster0.fxlwlgn.mongodb.net/mern-stack?retryWrites=true&w=majority`
-//       process.env.MONGO_URL
-//     );
-
-//     app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
   .then((res) => console.log("Connected to DB"))
@@ -58,4 +27,19 @@ app.listen(process.env.PORT, (error) => {
     ? console.log(error)
     : console.log(`Listening port ${process.env.PORT}`);
 });
-// start();
+
+// Middleware
+app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.static("uploads"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is already Connected");
+});
+
+// http://localhost:3002
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
