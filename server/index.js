31b3dcbf600739f08c1bfb6ca2223ba62ddb432d
Armen <armen.baghdasaryan91@gmail.com>
2022-12-31 20:28:38 +0400

    feature/test changes

--- server/index.js
+++ server/index.js
@@ -17,6 +17,23 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
+// Middleware
+app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.static("uploads"));
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Server is already Connected");
+});
+
+// http://localhost:3002
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
   .then((res) => console.log("Connected to DB"))
@@ -27,19 +44,3 @@ app.listen(process.env.PORT, (error) => {
     ? console.log(error)
     : console.log(`Listening port ${process.env.PORT}`);
 });
-
-// Middleware
-app.use(cors());
-app.use(fileUpload());
-app.use(express.json());
-app.use(express.static("uploads"));
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Server is already Connected");
-});
-
-// http://localhost:3002
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/comments", commentRoute);
